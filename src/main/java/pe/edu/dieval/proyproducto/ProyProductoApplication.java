5d19725e99830ad56a58e88aa7cfef30c37cf76b
DEValdivieso <omarjet30@gmail.com>
2025-10-14 13:17:15 -0500

    first version

---
+++ src/main/java/pe/edu/dieval/proyproducto/ProyProductoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.dieval.proyproducto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyProductoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyProductoApplication.class, args);
+    }
+
+}
